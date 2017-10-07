97c1f50ca8d4c6a555e9edf7cd644de935183dfc
pythondu <32488520+pythondu@users.noreply.github.com>
2017-10-07 15:43:21 +0800

    结束单一的python脚本进程
    结束单一的python脚本进程

--- picture/pid.py
+++ picture/pid.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*    
-  
+'''
+结束单一的python脚本进程
+'''
+#导入包
 import os
-import time
-import subprocess
+import time #定时模块
+import subprocess #执行终端命令模块
 
+#将当前系统中执行python脚本的进程信息输出到临时文件文件
 os.system('ps -e | grep python >jincheng.txt')
 
 #time.sleep(1)
 
+#打开临时文件
 f = open('jincheng.txt','r')
-
+#读取临时文件并提取其中的数据
 string=f.read()[1:5]
 
 #pid = int(string)
@@ -19,8 +24,11 @@ string=f.read()[1:5]
 
 #string='-SIGINT '+string
 
+#杀死进程
 subprocess.call(['kill','-2',string])
 
+#关闭文件IO
 f.close()
 
+#删除临时文件
 os.remove('./jincheng.txt')
