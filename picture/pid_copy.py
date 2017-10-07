8b2f79c51c994d6fa3a0d2a718ab4061b1109d21
pythondu <32488520+pythondu@users.noreply.github.com>
2017-10-07 15:47:22 +0800

    同时结束多个的python脚本进程
    同时结束多个的python脚本进程

--- picture/pid_copy.py
+++ picture/pid_copy.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*    
-  
+"""
+同时结束多个的python脚本进程
+"""
+#导入相关的包
 import os
 import time
 import subprocess
 
+#将当前系统中执行python脚本的进程信息输出到临时文件文件
 os.system('ps -e | grep python >jincheng.txt')
 
 #time.sleep(1)
 
-
+#打开临时文件并读取每一行的数据
 with open('jincheng.txt','r') as f:
 	lis=f.readlines()
 
-
+#遍历每一行的数据
 for li in lis:
 	try:
+		#截取PID并将其杀死
 		string=int(li[0:5])
 		string=str(string)
 		subprocess.call(['kill','-2',string])
@@ -32,5 +37,5 @@ for li in lis:
 #subprocess.call(['kill','-2',string])
 
 #f.close()
-
+#关闭文件io
 os.remove('./jincheng.txt')
